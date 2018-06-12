181fa661a9e47da86dbda080a6325c610ba10424
DudaFranca <mariaeduardafrancasantos@gmail.com>
2018-06-12 19:13:40 -0300

    atualizacao do template

--- cads_usuario.php
+++ cads_usuario.php
@@ -64,17 +64,67 @@
 			<!-- /.container-fluid --> 
 		</nav>
 		<div class="container">
-		
-	<form action="processa_usu.php" method="POST">
-		<input type="text" name="nome" placeholder="Nome">
-		<input type="text" name="sobrenome" placeholder="Sobrenome">
-		<input type="text" name="numero" placeholder="WhatsApp">
-		<input type="text" name="login" placeholder="Login">
-		<input type="password" name="senha" placeholder="Senha">
-		<input type="submit" value="Cadastrar">
-	</form>
-	<a href="login.php">Faça Login</a>
-	<br>
-	<button class="button" onclick="window.location='index.php'">Voltar</button>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<div id="contact" class="text-center">
+			<div class="container">
+    			<div class="section-title center">
+      				<h2>Cadastre-se</h2>
+      					<hr>
+    			</div>
+			</div>
+			<div class="col-md-8 col-md-offset-2">
+
+		<form action="processa_usu.php" method="POST">
+			<div class="row">
+				
+				<div class="col-md-6">
+            		<div class="form-group">
+						<input type="text" name="nome" placeholder="Nome" class="form-control" required="required>
+						<p class="help-block text-danger"></p>
+					</div>
+				</div>
+
+				<div class="col-md-6">
+					<div class="form-group">
+						<input type="text" name="sobrenome" placeholder="Sobrenome" class="form-control" required="required">
+						<p class="help-block text-danger"></p>
+					</div>
+				</div>
+
+				<div class="col-md-6">
+					<div class="form-group">
+						<input type="text" name="numero" placeholder="WhatsApp" class="form-control" required="required">
+						<p class="help-block text-danger"></p>
+					</div>
+				</div>
+
+				<div class="col-md-6">
+					<div class="form-group">
+						<input type="text" name="login" placeholder="Login" class="form-control" required="required">
+						<p class="help-block text-danger"></p>
+					</div>
+				</div>
+
+				<div class="col-md-6">
+					<div class="form-group">
+						<input type="password" name="senha" placeholder="Senha" class="form-control" required="required">
+						<p class="help-block text-danger"></p>
+					</div>
+				</div>
+
+				<button type="submit" class="btn btn-custom btn-lg" style="margin-top: 20%;
+				margin-left: -100%;">Cadastrar</button>
+			</div>
+			<br>
+			<br>
+			<a href="login.php">Faça Login</a>
+		</form>
+		<br>
+
+		<a href="indexcss.php" onclick="window.location='indexcss.php'">Voltar</a>
 </body>
 </html>
